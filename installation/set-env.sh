7697f022edf3b5d32c5e1565c9ceaf8c7d50f03b
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-01-30 20:24:20 +0000

    Troubleshooting strigo connectvity issues

--- installation/set-env.sh
+++ installation/set-env.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 export PUBLICIP=$(curl -s http://checkip.amazonaws.com)
-export KONG_ADMIN_API_URI=http://$PUBLICIP:8001
-export KONG_ADMIN_GUI_URL=http://$PUBLICIP:8002
-export KONG_PORTAL_GUI_HOST=http://$PUBLICIP:8003
-export KONG_PORTAL_API_URL=http://$PUBLICIP:8004
+
+export DYNDYNAMIC_FQDN=$(echo $STRIGO_RESOURCE_DNS)
+export KONG_ADMIN_API_URI=http://$DYNAMIC_FQDN:8001
+export KONG_ADMIN_GUI_URL=http://$DYNAMIC_FQDN:8002
+export KONG_PORTAL_GUI_HOST=http://$DYNAMIC_FQDN:8003
+export KONG_PORTAL_API_URL=http://$DYNAMIC_FQDN:8004
+
+# export KONG_ADMIN_API_URI=http://$PUBLICIP:8001
+# export KONG_ADMIN_GUI_URL=http://$PUBLICIP:8002
+# export KONG_PORTAL_GUI_HOST=http://$PUBLICIP:8003
+# export KONG_PORTAL_API_URL=http://$PUBLICIP:8004
+
+
+
 
 # export KONG_LICENSE_DATA=$(cat /usr/local/kong/license.json)
