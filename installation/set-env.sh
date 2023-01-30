792ea052b121684983c512bc2d93809f556c3f25
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-01-30 15:55:32 +0000

    Troubleshooting strigo connectvity issues

--- installation/set-env.sh
+++ installation/set-env.sh
@@ -3,5 +3,7 @@
 export PUBLICIP=$(curl -s http://checkip.amazonaws.com)
 export KONG_ADMIN_API_URI=http://$PUBLICIP:8001
 export KONG_ADMIN_GUI_URL=http://$PUBLICIP:8002
+export KONG_PORTAL_GUI_HOST=http://$PUBLICIP:8003
+export KONG_PORTAL_API_URL=http://$PUBLICIP:8004
 
-# echo "$(curl -s http://checkip.amazonaws.com)  kongcluster" >> /etc/hosts
+# export KONG_LICENSE_DATA=$(cat /usr/local/kong/license.json)
