716a18c8dc94ea7ce27da9f700791cb5d3d2a0a1
John Fitzpatrick <jhnftzptrck@gmail.com>
2023-02-26 20:38:59 +0000

    Update set-env.sh

--- installation/set-env.sh
+++ installation/set-env.sh
@@ -7,5 +7,5 @@ export KONG_ADMIN_API_URI=http://$DYNAMIC_FQDN:8001
 export KONG_ADMIN_GUI_URL=http://$DYNAMIC_FQDN:8002
 export KONG_PORTAL_GUI_HOST=$DYNAMIC_FQDN:8003
 export KONG_PORTAL_API_URL=http://$DYNAMIC_FQDN:8004
-
+export KONG_PROXY_URI=http://$DYNAMIC_FQDN:8000
 # export KONG_LICENSE_DATA=$(cat /usr/local/kong/license.json)
