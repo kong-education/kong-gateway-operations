c75b43a61e5ec81d07304f2afd0d4d198ddf33d9
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-02-27 09:45:26 +0000

    dev

--- vitals/configure_prometheus_grafana.sh
+++ vitals/configure_prometheus_grafana.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+DOCKERCOMPOSEFILE="~/kong-gateway-operations/installation/docker-compose.yaml"
 
-yq -i '.services.kong-cp.environment.KONG_VITALS_STRATEGY = "prometheus"' docker-compose.yaml
-yq -i '.services.kong-cp.environment.KONG_VITALS_STATSD_ADDRESS = "statsd:9125"' docker-compose.yaml
-yq -i '.services.kong-cp.environment.KONG_VITALS_TSDB_ADDRESS = "prometheus:9090"' docker-compose.yaml
+yq -i '.services.kong-cp.environment.KONG_VITALS_STRATEGY = "prometheus"' $DOCKERCOMPOSEFILE
+yq -i '.services.kong-cp.environment.KONG_VITALS_STATSD_ADDRESS = "statsd:9125"' $DOCKERCOMPOSEFILE
+yq -i '.services.kong-cp.environment.KONG_VITALS_TSDB_ADDRESS = "prometheus:9090"' $DOCKERCOMPOSEFILE
 
-yq -i '.services.kong-dp.environment.KONG_VITALS_STRATEGY = "prometheus"' docker-compose.yaml
-yq -i '.services.kong-dp.environment.KONG_VITALS_STATSD_ADDRESS = "statsd:9125"' docker-compose.yaml
-yq -i '.services.kong-dp.environment.KONG_VITALS_TSDB_ADDRESS = "prometheus:9090"' docker-compose.yaml
+yq -i '.services.kong-dp.environment.KONG_VITALS_STRATEGY = "prometheus"' $DOCKERCOMPOSEFILE
+yq -i '.services.kong-dp.environment.KONG_VITALS_STATSD_ADDRESS = "statsd:9125"' $DOCKERCOMPOSEFILE
+yq -i '.services.kong-dp.environment.KONG_VITALS_TSDB_ADDRESS = "prometheus:9090"' $DOCKERCOMPOSEFILE
