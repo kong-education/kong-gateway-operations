8bddeba1d5ae9532e8c6ccdd3419a3733abd8ad4
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-03-09 18:49:06 +0000

    dev

--- vitals/configure_prometheus_grafana.sh
+++ vitals/configure_prometheus_grafana.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKERCOMPOSEFILE="~/kong-gateway-operations/installation/docker-compose.yaml"
+DOCKERCOMPOSEFILE="/home/ubuntu/kong-gateway-operations/installation/docker-compose.yaml"
 
 yq -i '.services.kong-cp.environment.KONG_VITALS_STRATEGY = "prometheus"' $DOCKERCOMPOSEFILE
 yq -i '.services.kong-cp.environment.KONG_VITALS_STATSD_ADDRESS = "statsd:9125"' $DOCKERCOMPOSEFILE
