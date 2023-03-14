609044ba27e5c5f3b3168eae260cf92f21aeb563
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-03-14 10:07:16 +0000

    dev

--- metrics/launch_elk.sh
+++ metrics/launch_elk.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Copy Elk Config into shared volume with DinD
-cp -R elk /srv/shared
+# cp -R elk /srv/shared
 
 # Put license into a envionment variable so it is loaded by the docker compose
-export KONG_LICENSE=$(cat /etc/kong/license.json)
+export KONG_LICENSE=$(cat /usr/local/kong/license.json)
 
 
 echo "Preloading images into docker, this will take aproximatly three minutes"
